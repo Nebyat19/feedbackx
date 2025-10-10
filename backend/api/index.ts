36e7654518370a4f0df2b4ca2c40a563b496d9a5
nebyat19 <nebyat77bekele@yahoo.com>
2025-10-10 17:53:45 +0300

    fix : auth

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -12,6 +12,7 @@ server.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 )
 // Handle preflight requests automatically
