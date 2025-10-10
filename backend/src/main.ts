42530a9fb37c9bb290a0d7b43e090e8ca8cef8cc
nebyat19 <nebyat77bekele@yahoo.com>
2025-10-10 11:50:42 +0300

    fix: add origin https://api.feedbackx.me/

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:3000",
-      "https://feedbackx.vercel.app", 
+      "https://feedbackx.vercel.app", "https://api.feedbackx.me"
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
