0756b3e27ea05b2c76ad018f473f3f06857da038
nebyat19 <nebyat77bekele@yahoo.com>
2025-10-14 07:29:38 +0300

    fix: change smtp to api

--- backend/src/lib/mail.service.ts
+++ backend/src/lib/mail.service.ts
@@ -1,19 +1,25 @@
-const nodemailer = require("nodemailer")
+const Brevo = require("@getbrevo/brevo");
 
-export const transporter = nodemailer.createTransport({
-  host: process.env.BREVO_HOST,
-  port: process.env.BREVO_PORT,
-  auth: {
-    user: process.env.BREVO_USER,
-    pass: process.env.BREVO_PASS
-  }
-})
+const apiInstance = new Brevo.TransactionalEmailsApi();
+apiInstance.setApiKey(
+  Brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
 
 export async function sendEmail(to, subject, html) {
-  await transporter.sendMail({
-    from: "no-reply@mail.feedbackx.me",
-    to,
+  const emailData = {
+    sender: { name: "FeedbackX", email: "no-reply@mail.feedbackx.me" },
+    to: [{ email: to }],
     subject,
-    html
-  })
+    htmlContent: html,
+  };
+
+  try {
+    await apiInstance.sendTransacEmail(emailData);
+    console.log(`Email sent successfully to: ${to}`);
+  } catch (error) {
+    console.error("Email sending failed:", error.message);
+  }
 }
+
+
